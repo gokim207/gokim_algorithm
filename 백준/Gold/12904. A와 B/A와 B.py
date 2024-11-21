328ed816171d096586a41c3ad2a56fa4332dc317
김건오 <165020966+gokim207@users.noreply.github.com>
2024-11-21 22:51:31 +0900

    [Gold V] Title: A와 B, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.py
@@ -0,0 +1,17 @@
+a = input()
+b = input()
+while True:
+    if(a==b):
+        break
+    if (len(b) == 0):
+        print("0")
+        exit()
+    if (b[-1] == 'A'):
+        b = b[:-1]
+    elif (b[-1] == 'B'):
+        b = b[::-1]
+        b = b[1:]
+    else:
+        print("0")
+        exit()
+print("1")
