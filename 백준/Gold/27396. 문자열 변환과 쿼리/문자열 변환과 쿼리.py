3abd2adb5db6cd66a492d80153043ec55b0e249d
김건오 <165020966+gokim207@users.noreply.github.com>
2024-12-04 12:13:51 +0900

    [Gold V] Title: 문자열 변환과 쿼리, Time: 360 ms, Memory: 111104 KB, Score: 30 point -BaekjoonHub

--- 백준/Gold/27396. 문자열 변환과 쿼리/문자열 변환과 쿼리.py
+++ 백준/Gold/27396. 문자열 변환과 쿼리/문자열 변환과 쿼리.py
@@ -1,7 +1,12 @@
+import sys
+input = sys.stdin.readline
 a,b = input().split()
+
 for i in range(int(b)):
     x = input().split()
-    if x[0] == '2':
-        print(a)
-    else:
-        a = a.replace(x[1], x[2])
+    if x[0] == '1':
+        if x[1] == x[2] or x[1] not in a:
+            continue
+        a = a.replace(x[1],x[2], a.count(x[1]))
+        continue
+    print(a)
