df8330a6afa36ee6e3984b28d0937f7ab241658f
김건오 <165020966+gokim207@users.noreply.github.com>
2024-12-17 09:31:13 +0900

    [Bronze III] Title: 학술대회 참가신청, Time: 32 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.py
@@ -0,0 +1,7 @@
+digit = ['social', 'history', 'language', 'literacy']
+data = ['bigdata', 'public', 'society']
+a = input()
+if 'social' in a or 'history' in a or 'language' in a or 'literacy' in a:
+    print('digital humanities')
+else:
+    print('public bigdata')
