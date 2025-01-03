ad20ee13209734071b11c3d4ab0ed703891997d1
김건오 <165020966+gokim207@users.noreply.github.com>
2025-01-03 15:16:49 +0900

    [Bronze II] Title: 영리한 아리의 포탈 타기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/25165. 영리한 아리의 포탈 타기/영리한 아리의 포탈 타기.py
+++ 백준/Bronze/25165. 영리한 아리의 포탈 타기/영리한 아리의 포탈 타기.py
@@ -4,23 +4,17 @@ c,r = map(int,input().split())
 
 if d == 0:
     if n%2 == 0:
-        if c == 1 and r > x:
-            print('YES!')
-        else:
-            print('NO...')
+        print('NO...')
     else:
-        if c == n or (c == 1 and r > x):
+        if c == n:
             print('YES!')
         else:
             print('NO...')
 else:
     if n%2 == 0:
-        if c == n or (c == 1 and r < x):
+        if c == n:
             print('YES!')
         else:
             print('NO...')
     else:
-        if c == 1 and r < x:
-            print('YES!')
-        else:
-            print('NO...')
+        print('NO...')
