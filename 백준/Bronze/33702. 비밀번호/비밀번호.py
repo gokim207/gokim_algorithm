111ba83196d663dd9ee1388f7a86ad00a0ed1b6e
김건오 <165020966+gokim207@users.noreply.github.com>
2025-04-03 14:57:11 +0900

    [Bronze II] Title: 비밀번호, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/33702. 비밀번호/비밀번호.py
@@ -0,0 +1,19 @@
+n = int(input())
+if n == 1:
+    print(8)
+elif n == 2:
+    print(0)
+elif n == 3:
+    print(8)
+elif n == 4:
+    print(0)
+elif n == 5:
+    print(8)
+elif n == 6:
+    print(0)
+elif n == 7:
+    print(8)
+elif n == 8:
+    print(0)
+else:
+    print(8)
